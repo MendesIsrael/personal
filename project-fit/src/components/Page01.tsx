6468ed02479ff42e4721d10525bf551b8e9e53e0
Israel Mendes <101142371+MendesIsrael@users.noreply.github.com>
2022-11-08 17:55:07 -0300

    montando a estrutura

---
+++ project-fit/src/components/Page01.tsx
@@ -0,0 +1,24 @@
+import './Page01.module.css'
+
+export function Page01() {
+  return (
+    <section>
+      <header>
+        <div>menu</div>
+        <div>foto da logo</div>
+        <h1>Jereimas Lopes</h1>
+      </header>
+      <body>
+        <div>analise</div>
+        <div>info</div>
+        <div>prescriçao</div>
+        <div>nutriçao</div>
+      </body>
+      <footer>
+        <p>instagran
+          zap
+        </p>
+      </footer>
+    </section>       
+  )
+}
